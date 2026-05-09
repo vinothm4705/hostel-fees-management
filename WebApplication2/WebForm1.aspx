8242a5937d22f0733fae5b805222dadb9ab5c556
Vinoth M <vinxth040705@gmail.com>
2026-05-10 00:42:59 +0530

    Initial Commit

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,117 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+<html>
+<head id="Head1" runat="server">
+    <title>Hostel Fees Calculation System</title>
+
+    
+</head>
+
+<body>
+<center>
+<form id="form1" runat="server">
+
+<h2>HOSTEL FEES CALCULATION SYSTEM</h2>
+
+
+<table>
+<tr>
+<td>STUDENT ID:</td>
+<td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></td>
+</tr>
+
+<tr>
+<td>STUDENT NAME:</td>
+<td><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+</tr>
+
+<tr>
+<td>ROOM TYPE:</td>
+<td>
+<asp:DropDownList ID="DropDownList1" runat="server">
+<asp:ListItem Text="-- Select --" Value=""></asp:ListItem>
+<asp:ListItem Text="Single Room-4000" Value="4000"></asp:ListItem>
+<asp:ListItem Text="Double Sharing-3000" Value="3000"></asp:ListItem>
+<asp:ListItem Text="Triple Sharing-2500" Value="2500"></asp:ListItem>
+</asp:DropDownList>
+</td>
+</tr>
+
+<tr>
+<td>MONTHS:</td>
+<td><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></td>
+</tr>
+</table>
+
+<asp:Button ID="Button1" runat="server" Text="CALCULATE" 
+    CssClass="calc-btn" OnClick="Button1_Click" />
+
+
+<br />
+
+
+<br />
+
+
+
+<asp:Button ID="Button2" runat="server" Text="INSERT" OnClick="Button2_Click" CssClass="style1" />
+<asp:Button ID="Button3" runat="server" Text="UPDATE" OnClick="Button3_Click" CssClass="style1" />
+<asp:Button ID="Button4" runat="server" Text="DELETE" OnClick="Button4_Click" CssClass="style1" />
+<asp:Button ID="Button5" runat="server" Text="SELECT" OnClick="Button5_Click" CssClass="style1" />
+<asp:Button ID="Button6" runat="server" Text="SHOW ALL" OnClick="Button6_Click" CssClass="style1" />
+
+
+<br />
+
+
+<br />
+<asp:Literal ID="LiteralFees" runat="server"></asp:Literal>
+
+<br />
+<br />
+<asp:Label ID="LabelMessage" runat="server" 
+    style="color: #009900; font-weight: 700"></asp:Label>
+
+<br />
+<br />
+
+<div class="grid-container">
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+    DataKeyNames="StudentID" DataSourceID="SqlDataSource1">
+
+    <Columns>
+        <asp:BoundField DataField="StudentID" HeaderText="StudentID" ReadOnly="True" 
+            SortExpression="StudentID" />
+        <asp:BoundField DataField="StudentName" HeaderText="StudentName" 
+            SortExpression="StudentName" />
+        <asp:BoundField DataField="RoomType" HeaderText="RoomType" 
+            SortExpression="RoomType" />
+        <asp:BoundField DataField="MonthsStayed" HeaderText="MonthsStayed" 
+            SortExpression="MonthsStayed" />
+        <asp:BoundField DataField="RoomCharges" HeaderText="RoomCharges" 
+            SortExpression="RoomCharges" />
+        <asp:BoundField DataField="MessCharges" HeaderText="MessCharges" 
+            SortExpression="MessCharges" />
+        <asp:BoundField DataField="ElectricityCharges" HeaderText="ElectricityCharges" 
+            SortExpression="ElectricityCharges" />
+        <asp:BoundField DataField="Subtotal" HeaderText="Subtotal" 
+            SortExpression="Subtotal" />
+        <asp:BoundField DataField="ServiceCharge" HeaderText="ServiceCharge" 
+            SortExpression="ServiceCharge" />
+        <asp:BoundField DataField="TotalFee" HeaderText="TotalFee" 
+            SortExpression="TotalFee" />
+    </Columns>
+
+</asp:GridView>
+
+<asp:SqlDataSource ID="SqlDataSource1" runat="server"
+    ConnectionString="<%$ ConnectionStrings:Database1 %>"
+    SelectCommand="SELECT * FROM [HostelFees]">
+</asp:SqlDataSource>
+</div>
+
+</form>
+</center>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(
+        ConfigurationManager.ConnectionStrings["Database1"].ConnectionString);
+
+        double roomCharges, messCharges, electricityCharges, subTotal, serviceCharge, totalFee;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        void CalculateFee()
+        {
+            double rent = Convert.ToDouble(DropDownList1.SelectedValue);
+            int months = Convert.ToInt32(TextBox4.Text);
+
+            roomCharges = rent * months;
+            messCharges = 2000 * months;
+            electricityCharges = 500 * months;
+
+            subTotal = roomCharges + messCharges + electricityCharges;
+            serviceCharge = subTotal * 0.05;
+
+            totalFee = subTotal + serviceCharge;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CalculateFee();
+
+            LiteralFees.Text =
+                "Room Charges: " + roomCharges + "<br/>" +
+                "Mess Charges: " + messCharges + "<br/>" +
+                "Electricity Charges: " + electricityCharges + "<br/>" +
+                "Subtotal: " + subTotal + "<br/>" +
+                "Service Charge (5%): " + serviceCharge + "<br/><br/>" +
+                "<b>Total Fee: " + totalFee + "</b>";
+
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            CalculateFee();
+
+            con.Open();
+
+            string q = "insert into HostelFees values(@id,@name,@room,@months,@roomC,@mess,@ele,@sub,@service,@total)";
+            SqlCommand cmd = new SqlCommand(q, con);
+
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@room", DropDownList1.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@months", TextBox4.Text);
+            cmd.Parameters.AddWithValue("@roomC", roomCharges);
+            cmd.Parameters.AddWithValue("@mess", messCharges);
+            cmd.Parameters.AddWithValue("@ele", electricityCharges);
+            cmd.Parameters.AddWithValue("@sub", subTotal);
+            cmd.Parameters.AddWithValue("@service", serviceCharge);
+            cmd.Parameters.AddWithValue("@total", totalFee);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            LabelMessage.Text = "Record Inserted Successfully";
+
+            GridView1.DataBind();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            CalculateFee();
+
+            con.Open();
+
+            string q = "update HostelFees set StudentName=@name,RoomType=@room,MonthsStayed=@months,RoomCharges=@roomC,MessCharges=@mess,ElectricityCharges=@ele,Subtotal=@sub,ServiceCharge=@service,TotalFee=@total where StudentID=@id";
+            SqlCommand cmd = new SqlCommand(q, con);
+
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@name", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@room", DropDownList1.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@months", TextBox4.Text);
+            cmd.Parameters.AddWithValue("@roomC", roomCharges);
+            cmd.Parameters.AddWithValue("@mess", messCharges);
+            cmd.Parameters.AddWithValue("@ele", electricityCharges);
+            cmd.Parameters.AddWithValue("@sub", subTotal);
+            cmd.Parameters.AddWithValue("@service", serviceCharge);
+            cmd.Parameters.AddWithValue("@total", totalFee);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            LabelMessage.Text = "Record Updated Successfully";
+
+            GridView1.DataBind();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string q = "delete from HostelFees where StudentID=@id";
+            SqlCommand cmd = new SqlCommand(q, con);
+
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            LabelMessage.Text = "Record Deleted Successfully";
+
+            GridView1.DataBind();
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string q = "select * from HostelFees where StudentID=@id"; 
+            SqlCommand cmd = new SqlCommand(q, con);
+
+            cmd.Parameters.AddWithValue("@id", TextBox1.Text);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                TextBox2.Text = dr["StudentName"].ToString();
+                DropDownList1.SelectedItem.Text = dr["RoomType"].ToString();
+                TextBox4.Text = dr["MonthsStayed"].ToString();
+
+                LiteralFees.Text =
+                    "Room Charges: " + dr["RoomCharges"] + "<br/>" +
+                    "Mess Charges: " + dr["MessCharges"] + "<br/>" +
+                    "Electricity Charges: " + dr["ElectricityCharges"] + "<br/>" +
+                    "Subtotal: " + dr["Subtotal"] + "<br/>" +
+                    "Service Charge (5%): " + dr["ServiceCharge"] + "<br/><br/>" +
+                    "<b>Total Fee: " + dr["TotalFee"] + "</b>";
+            }
+
+            con.Close();
+
+            LabelMessage.Text = "Record Selected Successfully";
+
+            GridView1.DataBind();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+
+    }
+}
